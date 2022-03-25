54f0f330ecb92db886da158a7d027790fe782839
maoforero <maforeror@gmail.com>
2022-03-24 23:43:07 -0500

    creando navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <>
+    <nav>
+    <div>
+      <img src="" alt="main logo" />
+    </div>
+
+    <div>
+      <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
+    </div>
+    </nav>
+    </>
   )
 }
 
