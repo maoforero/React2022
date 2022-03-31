b0ef36173f143d1131567cb0008b665c7dd658c7
maoforero <maforeror@gmail.com>
2022-03-30 23:20:26 -0500

    implementando botones

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-const ItemListContainer = () => {
+export default function  ItemListContainer (){
+
+  const [numeroCount, setNumero] = useState(0);
+
+  function incrementoClick(){
+    setNumero(numeroCount+1);
+  }
+
+  function decrementoClick(){
+    setNumero(numeroCount--);
+  }
+
   return (
     <div>
       <h1>CURSO DE REACT CODERHOUSE</h1>
+      <p>{numeroCount}</p>
+      <div>
+        <button onClick={incrementoClick}>Incremento</button>
+        <button onclick={decrementoClick}>Decremento</button>
+      </div>
     </div>
   )
 }
-
-export default ItemListContainer
