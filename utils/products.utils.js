e178e4ba779496f90a4a449db58fad7b3579624c
maoforero <maforeror@gmail.com>
2022-04-03 23:34:10 -0500

    objetos item

---
+++ utils/products.utils.js
@@ -0,0 +1,44 @@
+const productos = [
+  {
+    id: 1,
+    name: "Producto 1",
+    price: '$100',
+    description: "Descripcion del producto 1",
+    image: "",
+  },
+  {
+    id: 2,
+    name: "Producto 2",
+    price: '$200',
+    description: "Descripcion del producto 2",
+    image: "",
+  },
+  {
+    id: 3,
+    name: "Producto 3",
+    price: '$300',
+    description: "Descripcion del producto 3",
+    image: "",
+  },
+  {
+    id: 4,
+    name: "Producto 4",
+    price: '$400',
+    description: "Descripcion del producto 4",
+    image: "",
+  },
+  {
+    id: 5,
+    name: "Producto 5",
+    price: '$500',
+    description: "Descripcion del producto 5",
+    image: "",
+  },
+  {
+    id: 6,
+    name: "Producto 6",
+    price: '$600',
+    description: "Descripcion del producto 6",
+    image: "",
+  },
+]
