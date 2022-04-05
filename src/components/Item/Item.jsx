7fee948661dceef68e9b0ae32e1e39c205649277
maoforero <maforeror@gmail.com>
2022-04-05 00:09:57 -0500

    itemlist props

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Item({id, name, price, description, image}) {
   return (
-    <div className='container__item'>
+    <div key={id} className='container__item'>
       <div className="container__item--img">
         <img src={image} alt={name}/>
       </div>
