ec7336d717f6883380cdb4ea2ae766645124fd29
maoforero <maforeror@gmail.com>
2022-04-04 23:56:58 -0500

    item estructura

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function Item() {
+function Item({id, name, price, description, image}) {
   return (
-    <div>Item</div>
+    <div className='container__item'>
+      <div className="container__item--img">
+        <img src={image} alt={name}/>
+      </div>
+      <div className="container__item--info">
+        <h2>{name}</h2>
+        <p>{price}</p>
+      </div>
+    </div>
   )
 }
 
