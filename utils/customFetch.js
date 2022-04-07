cd76d5ce76ddd619dc91a3559c6b0d9ad94eba10
maoforero <maforeror@gmail.com>
2022-04-06 23:55:58 -0500

    Custom Fetch

---
+++ utils/customFetch.js
@@ -0,0 +1,11 @@
+let isOk = true;
+
+const customFetch = (time, task) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isOk) {
+        resolve(task);
+      } else {
+        reject('Error');
+      }
+    }, time)})};
