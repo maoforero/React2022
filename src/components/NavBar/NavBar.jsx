9da1a43b9dee1d58c0097efddf09c8c91b2cb571
maoforero <maforeror@gmail.com>
2022-04-07 16:15:10 -0500

    addind scss

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,37 +1,18 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import { AppBar, IconButton, Button, Toolbar } from '@material-ui/core';
 import CartWidget from '../CartWidget/CartWidget';
+import '../NavBar/navBar.css'
 
 const navbar = () => {
   return (
-    <>
-      <Container>
-        <AppBar position='static'>
-          <Toolbar>
-            <IconButton edge='start' color='inherit' arial-table='menu' />
-            <Button color="inherit" to='/'>
-              <p>Home</p>
-            </Button>
-
-            <Button color="inherit" to='/'>
-              <p>List</p>
-            </Button>
-              
-            <Button color="inherit" to='/'>
-              <p>More</p>
-            </Button>
-
-            <Button color="inherit" to='/'>
-              <p>Cart</p>
-            </Button>
-            <CartWidget/>
-          </Toolbar>
-
-        </AppBar>
-
-    </Container>
-    </>
+      <nav className='container__navBar'>
+          <ul className='container__navBar--list'>
+            <li className="navBar--list">Home</li>
+            <li className="navBar--list">Menu</li>
+            <li className="navBar--list">News</li>
+            <li className="navBar--list">Info</li>
+          </ul>
+          <CartWidget/>
+      </nav>
   )
 }
 
