6aa2aa575b88556024f62fa18e3118a1d98a0501
maoforero <maforeror@gmail.com>
2022-04-07 00:42:50 -0500

    fixing render img

--- src/utils/products.js
+++ src/utils/products.js
@@ -11,35 +11,36 @@ const productos = [
     name: "Producto 2",
     price: '$200',
     description: "Descripcion del producto 2",
-    https: "//random.imagecdn.app/240/240"
+    image: "https://random.imagecdn.app/240/240"
   },
   {
     id: 3,
     name: "Producto 3",
     price: '$300',
     description: "Descripcion del producto 3",
-    https: "//random.imagecdn.app/240/240"
+    image: "https://random.imagecdn.app/240/240"
   },
   {
     id: 4,
     name: "Producto 4",
     price: '$400',
     description: "Descripcion del producto 4",
-    https: "//random.imagecdn.app/240/240"
+    image: "https://random.imagecdn.app/240/240"
   },
   {
     id: 5,
     name: "Producto 5",
     price: '$500',
     description: "Descripcion del producto 5",
-    https: "//random.imagecdn.app/240/240"
+    image: "https://random.imagecdn.app/240/240"
   },
   {
     id: 6,
     name: "Producto 6",
     price: '$600',
     description: "Descripcion del producto 6",
-    https: "//random.imagecdn.app/240/240"  },
+    image: "https://random.imagecdn.app/240/240"
+  },
 ]
 
 export default productos;
