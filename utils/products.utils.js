e73dd7bad53f069e4273264d03bbbb71ade84375
maoforero <maforeror@gmail.com>
2022-04-06 23:40:21 -0500

    json con url random pictures

--- utils/products.utils.js
+++ utils/products.utils.js
@@ -4,41 +4,40 @@ const productos = [
     name: "Producto 1",
     price: '$100',
     description: "Descripcion del producto 1",
-    image: "",
+    image: "https://random.imagecdn.app/240/240",
   },
   {
     id: 2,
     name: "Producto 2",
     price: '$200',
     description: "Descripcion del producto 2",
-    image: "",
+    https: "//random.imagecdn.app/240/240"
   },
   {
     id: 3,
     name: "Producto 3",
     price: '$300',
     description: "Descripcion del producto 3",
-    image: "",
+    https: "//random.imagecdn.app/240/240"
   },
   {
     id: 4,
     name: "Producto 4",
     price: '$400',
     description: "Descripcion del producto 4",
-    image: "",
+    https: "//random.imagecdn.app/240/240"
   },
   {
     id: 5,
     name: "Producto 5",
     price: '$500',
     description: "Descripcion del producto 5",
-    image: "",
+    https: "//random.imagecdn.app/240/240"
   },
   {
     id: 6,
     name: "Producto 6",
     price: '$600',
     description: "Descripcion del producto 6",
-    image: "",
-  },
+    https: "//random.imagecdn.app/240/240"  },
 ]
