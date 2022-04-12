ba46bb1f137b57524567b201c173c7b470c4d79f
maoforero <maforeror@gmail.com>
2022-04-12 09:43:50 -0500

    ajustes APP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <ItemListContainer/>
+      <BrowserRouter>
+        <routes>
+          <div>
+            <route exact path="/home" element={Home}/>
+            <route exact path="/menu" element={menu}/>
+            <route path="*" element={NotFound}/>
+          </div>
+        </routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-<<<<<<< HEAD
-    <div>StarBucks</div>
-=======
     <div>Home</div>
->>>>>>> 9c6dd0e4c88558f983bad01849ce50614da7f7e5
   )
 }
 
