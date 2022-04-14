3d0a27f1a3ae7689f2b9a0955bc0327b44e26c8d
maoforero <maforeror@gmail.com>
2022-04-14 01:48:18 -0500

    primer entrega del proyecto Final

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import Count from '../Count/Count';
+import '../../Styles/components/ItemDetail/ItemDetail.css'
 
-const ItemDetail = (itemSelec) => {
+const ItemDetail = (props) => {
+
+  let idItem = props;
+
   return (
-    <div>
-      <h1>ITEM</h1>
-
-      <div className="containerItemDetail">
-        <div className="container__ItemDetail--img">
-          <img src="" alt="" className='ItemDetail--img'/>
+    <div className="containerItemDetail">
+      <div className="container__ItemDetail--img">
+        <img src={idItem.img} alt="" className='ItemDetail--img'/>
+      </div>
+      <div className="container__ItemDetail--info">
+        <h6 className="ItemDetail--infoTitle">{idItem.name}</h6>
+        <p className="ItemDetail--infoDescription">{idItem.info}</p>
+        <p className="ItemDetail--infoPrice">{idItem.price}</p>
+      </div>
+      <div className="ItemDetail--buttonsIteractions">
+        <div className="countInteractions">
+          <p className="count">numeros</p>
         </div>
-        <div className="container__ItemDetail--info">
-          <h6 className="ItemDetail--infoTitle">{itemSelec.name}</h6>
-          <p className="ItemDetail--infoDescription">{itemSelec.price}</p>
-        </div>
-        <div className="ItemDetail--buttonsIteractions">
-          <div className="countInteractions">
-            <p className="count">numeros</p>
-          </div>
-          <div className="buttonsInteractions">
-            <Count/>
-          </div>
+        <div className="buttonsInteractions">
+          <Count/>
         </div>
       </div>
     </div>
   )
 }
 
-export default ItemDetail
+export default ItemDetail;
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,26 +1,49 @@
-import React, {useEffect, useState}from 'react';
-import productos from '../../utils/products'
-import ItemDetail from '../ItemDetail/ItemDetail';
-import { Link, useParams } from 'react-router-dom';
-import customFetch from '../../utils/customFetch';
+import React, { useEffect, useState } from "react";
+import productos from "../../utils/products";
+import ItemDetail from "../ItemDetail/ItemDetail";
+import { useParams } from "react-router-dom";
+import customFetch from "../../utils/customFetch";
 
 const ItemDetailContainer = () => {
-
   const [product, setProduct] = useState([]);
 
-  let { idItem } = useParams();
-  
-  useEffect(() => {
-    customFetch(1000, productos)
-      .then(res => setProduct(res.find(i => i.id === idItem)));
-  })
+  let idItem = useParams();
 
+  useEffect(() => {
+    customFetch(1000, productos).then((res) =>
+      setProduct(res.filter((i) => i.id === idItem.id))
+    );
+  },[]);
+
+  let itemId;
+  let itemName;
+  let itemPrice;
+  let itemInfo;
+  let itemImg;
+
+  let validateProduct = (product) => {
+    if(product.length !== 0){
+      itemId = product[0].id;
+      itemName = product[0].name;
+      itemPrice = product[0].price;
+      itemInfo = product[0].description;
+      itemImg = product[0].image;   
+    }
+  }
+  validateProduct(product)
 
   return (
     <div>
-        <ItemDetail key={product.id} itemSelect={product}/>
+      <ItemDetail
+        key={itemId}
+        id={itemId}
+        name={itemName}
+        price={itemPrice}
+        info={itemInfo}
+        img={itemImg}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ItemDetailContainer
+export default ItemDetailContainer;
