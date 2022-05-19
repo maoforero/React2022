6c173a05871022ce9578cc0924fddd06b2f822c5
maoforero <maforeror@gmail.com>
2022-05-19 00:00:29 -0500

    estilos 404

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
+import logo256 from '../Assets/logo/starbucks256.png'
 import '../Styles/components/footer/footer.css'
 
-const footer = () => {
+const Footer = () => {
   return (
-    <footer>
-    <div class="cointer__footer__description">
-        <div class="footer__description--logo">
-            <img src="https://www.misiontic2022.gov.co/746/channels-645_logo_misiontic.png" alt="logo mision tic"/>
+    <footer className='container__footer'>
+    <div className="cointer__footer__description">
+        <div className="footer__description--logo">
+            <img src={logo256} alt="logo starbucks"/>
         </div>
-        <div class="footer__description__info">
+        <div className="footer__description__info">
             <p>React project focused on the creation of a virtual store that mimics Starbucks. All its structure was developed during the React course at CoderHouse. </p>
         </div>
     </div>
-    <div class="cointer__footer__team">
+    <div className="cointer__footer__team">
         <h6>Autor</h6>
-        <a href="#" class="footer__team--profile">Forero Mauro</a>
+        <a href="#" className="footer__team--profile">Forero Mauro</a>
     </div>
-    <div class="cointer__footer__media">
+    <div className="cointer__footer__media">
         <h6>Social Media</h6>
         <a href="#">Facebook</a>
         <a href="#">Twitter</a>
         <a href="#">Instagram</a>
-        <a href="#">Bitbucket</a>
+        <a href="https://github.com/maoforero">Github</a>
     </div>
 </footer>
   )
 }
 
-export default footer
+export default Footer
--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import imgNotfound from '../Assets/notFound/404.png';
 
 const NotFound = () => {
   return (
-    <div>Content Not Found</div>
+    <div className='contianer__notFound'>
+      <h1 className='notFound--title'>Ups! Es posible que la página que estás buscando no exista o haya sido eliminada</h1>
+      <div className='notFound--img'>
+        <img src={imgNotfound} alt="error 404 No encontrado" />
+      </div>
+    </div>
   )
 }
 
